8e4842768c70d23202a4f7c6ef3e8a4e2d9c2196
Troy M. Connor <troy0820@users.noreply.github.com>
2016-04-16 16:18:05 -0400

    [index]Testing no-fallthrough

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ console.log('eslint =>', eslint)
 const arrow = () => { return 'This is an arrow function' }
 
 console.log('arrow', arrow())
+
+const n = 3
+
+switch (n) {
+  case 1:
+  case 2:
+  case 3:
+  console.log('Case 3')
+}
