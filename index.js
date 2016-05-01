2f86f46d9fd28c28d7138af36d5c375f2470e7a7
Troy Connor <troy0820@users.noreply.github.com>
2016-05-01 10:12:42 -0400

    [index]delete comment

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const eslint = 'ESLINT is awesome!!!'
 
 console.log('eslint =>', eslint)
@@ -5,14 +6,3 @@ console.log('eslint =>', eslint)
 const arrow = () => { return 'This is an arrow function' }
 
 console.log('arrow', arrow())
-
-const n = 3
-
-switch (n) {
-  case 1:
-  case 2:
-  //this shouldn't break
-  //yup this shouldn't break at all
-  case 3:
-  console.log('Case 3')
-}
